42bdcc2949894bd7a692e232049d23a9af7f97c1
taniguchi-111 <h1202015031@hiratagakuen.onmicrosoft.com>
2021-10-26 16:29:39 +0900

    障害物をよける仕様の追加（未完成）

--- kaguro-ru/Assets/taniguchi/Clear.cs
+++ kaguro-ru/Assets/taniguchi/Clear.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Clear : MonoBehaviour
 {
     GameObject Furniture;
 
-
     void Start()
     {
-        Furniture = GameObject.Find("Furniture");
+        //‰Æ‹ï‚Ìî•ñ‚ğæ“¾
+        Furniture = GameObject.Find("furniture");
     }
 
     void Update()
     {
+        //‰Æ‹ï‚ªnull‚¾‚Á‚½‚çClearScene‚ÉˆÚ“®
         if(Furniture==null)
         {
-
+            SceneManager.LoadScene("Clear");
         }
     }
 }
--- kaguro-ru/Assets/taniguchi/Furniture.cs
+++ kaguro-ru/Assets/taniguchi/Furniture.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Furniture : MonoBehaviour
 {
     GameObject Player;
+    GameObject Obstacle;
     Rigidbody rigidbody;
     Player script;
     private float Gauge;
@@ -17,6 +18,7 @@ public class Furniture : MonoBehaviour
     {
         //プレイヤーの情報を探して取得
         Player = GameObject.Find("Player");
+        Obstacle = GameObject.Find("obstacle");
         script = Player.GetComponent<Player>();
         rigidbody = this.GetComponent<Rigidbody>();
     }
@@ -28,6 +30,29 @@ public class Furniture : MonoBehaviour
 
         //前方に移動
         this.rigidbody.velocity = new Vector3(0, 0, 1f);
+
+        //前方に障害物があると横に避ける（未完成）
+        ////Rayを飛ばして前方に物体があるか調べる
+        ////Rayの作成　　　　　　　↓Rayを飛ばす原点　　　↓Rayを飛ばす方向
+        //Ray ray = new Ray(transform.position, new Vector3(0, -1, 10));
+
+        //RaycastHit hit;
+        ////Rayの飛ばせる距離
+        //int distance = 20;
+
+        ////Ray可視化
+        //Debug.DrawLine(ray.origin, ray.direction * distance, Color.red);
+
+        ////もしRayにオブジェクトが衝突したら
+        ////                  ↓Ray  ↓Rayが当たったオブジェクト ↓距離
+        //if (Physics.Raycast(ray,out hit, distance))
+        //{
+        //    if(hit.collider.tag == "Obstacle")
+        //    {
+        //        this.rigidbody.velocity = new Vector3(1.0f, 0, 0);
+        //    }
+        //}
+
 
         //捕獲ゲージが設定された値を超えると自身を削除
         if (Gauge > Gauge_MAX)
