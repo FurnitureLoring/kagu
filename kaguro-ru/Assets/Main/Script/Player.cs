dd689680ad85a38f58caae45310ad6d3359c0ed4
taniguchi-111 <h1202015031@hiratagakuen.onmicrosoft.com>
2021-12-03 11:54:11 +0900

    主人公にアニメーションを追加

--- kaguro-ru/Assets/Main/Script/Player.cs
+++ kaguro-ru/Assets/Main/Script/Player.cs
@@ -5,19 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody rigidbody;
     GameObject furniture;           //家具情報の格納変数
     GameObject Goal;                //ゴール情報の格納変数
     Result result_s;                //リザルトスクリプトの情報格納変数
-
+    Animator Playeranimation;
     public float distance;          //距離
     float possible_distance = 1.5f; //捕獲可能距離
     public float gauge = 0;         //捕獲ゲージ
     float Speed = 3.5f;             //移動スピード
     float CaptureSpeed = 3.0f;      //捕獲可能距離内でのスピード
+    bool hole;                      //落とし穴判別用
 
     //テスト用
-    public bool hole;
+    public bool left;  //trueなら主人公が左に移動
+    public bool right; //trueなら主人公が右に移動
+
 
     void Start()
     {
@@ -28,9 +30,11 @@ public class Player : MonoBehaviour
         Goal = GameObject.Find("Goal");
         result_s = Goal.GetComponent<Result>();
 
-        rigidbody = this.GetComponent<Rigidbody>();
-
         hole = false;
+
+
+        left = false;
+        right = false;
     }
 
     void FixedUpdate()
@@ -39,44 +43,55 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += transform.right * Speed * Time.deltaTime;
+            right = true;
         }
         //左に移動
         if(Input.GetKey(KeyCode.A))
         {
             transform.position -= transform.right * Speed * Time.deltaTime;
+            left = true;
         }
-        //家具が存在するときに動作
+        //家具が存在するときと、落とし穴の上にいないときに動作
         if (furniture != null && hole==false)
         {
             //家具との距離を代入
             distance = Vector3.Distance(transform.position, furniture.transform.position);
 
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+
             //その場に停止
             if (Input.GetKey(KeyCode.S))
             {
-                transform.position = transform.position;
-            }
-            //前方に移動
-            //距離が離れていれば速度が速くなり、近ければ同速になる
-            else if (distance < possible_distance)
-            {
-                this.rigidbody.velocity = new Vector3(0, 0, CaptureSpeed);
-            }
-            else
-            {
-                this.rigidbody.velocity = new Vector3(0, 0, Speed);
-            }
+                transform.rotation = Quaternion.Euler(0, 180, 0);
 
-            //距離が設定された値より近いときに
-            //spaceキーを入力すると捕獲ゲージを増やす
-            if (Input.GetKey(KeyCode.Space))
-            {
-                if (distance <= possible_distance)
+                transform.position = transform.position;
+
+                //距離が設定された値より近くて、
+                //プレイヤーのZ座標が家具のZ座標より大きいときに、
+                //捕獲ゲージを増やす
+                if (distance <= possible_distance && transform.position.z >= furniture.transform.position.z)
                 {
                     //一秒ごとに+1される
                     gauge += Time.deltaTime;
                 }
             }
+            //前方に移動
+            //距離が離れていれば速度が速くなり、近ければ同速になる
+            else if (distance < possible_distance)
+            {
+                transform.position += transform.forward * CaptureSpeed * Time.deltaTime;
+            }
+            else
+            {
+                transform.position += transform.forward * Speed * Time.deltaTime;
+            }
+
+            ////距離が設定された値より近くて、
+            ////プレイヤーのZ座標が家具のZ座標より大きいときに、
+            ////spaceキーを入力すると捕獲ゲージを増やす
+            //if (Input.GetKey(KeyCode.Space))
+            //{
+            //}
         }
     }
 
@@ -92,11 +107,12 @@ public class Player : MonoBehaviour
         {
             this.GetComponent<BoxCollider>().isTrigger = true;
             hole = true;
-            StartCoroutine(Wait());
+            StartCoroutine(GameOverWait());
         }
     }
 
-    IEnumerator Wait()
+    //GameOverシーンに移動するコルーチン
+    IEnumerator GameOverWait()
     {
         yield return new WaitForSeconds(1.0f);
         SceneManager.LoadScene("GameOver");
