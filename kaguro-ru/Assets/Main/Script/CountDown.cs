7e583e950cae459d09d2ce3e6c9e6e8d8a6235ed
taniguchi-111 <h1202015031@hiratagakuen.onmicrosoft.com>
2021-12-17 12:32:05 +0900

    プログラムの修正

--- kaguro-ru/Assets/Main/Script/CountDown.cs
+++ kaguro-ru/Assets/Main/Script/CountDown.cs
@@ -35,6 +35,41 @@ public class CountDown : MonoBehaviour
 		StartCoroutine(CountdownCoroutine3());
 	}
 
+	IEnumerator CountdownCoroutine()
+	{
+		_imageMask.gameObject.SetActive(true);
+		_textCountdown.gameObject.SetActive(true);
+
+		_textCountdown.text = "3";
+		yield return new WaitForSeconds(1.0f);
+
+		_textCountdown.text = "2";
+		yield return new WaitForSeconds(1.0f);
+
+		_textCountdown.text = "1";
+		yield return new WaitForSeconds(1.0f);
+
+		_textCountdown.text = "GO!";
+		yield return new WaitForSeconds(1.0f);
+
+        if ("Stage1" == GameObject.Find("Stage1").name)
+        {
+			scenetrans.StageStart1();
+        }
+        if ("Stage2" == GameObject.Find("Stage2").name)
+        {
+			scenetrans.StageStart2();
+        }
+		if ("Stage3" == GameObject.Find("Stage3").name)
+		{
+			scenetrans.StageStart3();
+		}
+
+		_textCountdown.text = "";
+		_textCountdown.gameObject.SetActive(false);
+		_imageMask.gameObject.SetActive(false);
+	}
+
 	IEnumerator CountdownCoroutine1()
 	{
 		_imageMask.gameObject.SetActive(true);
