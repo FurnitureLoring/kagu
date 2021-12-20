2e09c54ec9271d9e9f9dea2b91a5a01651f96a3d
taniguchi-111 <h1202015031@hiratagakuen.onmicrosoft.com>
2021-12-20 16:26:41 +0900

    カメラの移動を追加

--- kaguro-ru/Assets/Main/Script/StageSelect.cs
+++ kaguro-ru/Assets/Main/Script/StageSelect.cs
@@ -1,19 +1,84 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class StageSelect: MonoBehaviour
+public class StageSelect : MonoBehaviour
 {
-    private float RotateSpeed = 0.5f;  //回転速度
+    GameObject[] Panel = new GameObject[3];     //StagePanel配列
+
+    public GameObject Camera;
 
     void Start()
     {
-
+        Panel[0] = GameObject.Find("StagePanel1");
+        Panel[1] = GameObject.Find("StagePanel2");
+        Panel[2] = GameObject.Find("StagePanel3");
     }
 
-    void FixedUpdate()
+    public void Back()
     {
-        //自身を回転させる
-        this.transform.Rotate(0.0f, RotateSpeed, 0.0f);
+        //StageSelectパネルが端を超えて移動しないように制御
+        if (Panel[0].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
+            Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                StartCoroutine(PanelBack(Panel[i]));
+                StartCoroutine(CameraBack(Camera));
+            }
+        }
+    }
+
+    public void Forward()
+    {
+        //StageSelectパネルが端を超えて移動しないように制御
+        if (Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
+            Panel[2].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                StartCoroutine(PanelForward(Panel[i]));
+                StartCoroutine(CameraForward(Camera));
+            }
+        }
+    }
+
+    //StageSelectパネルが左に移動
+    IEnumerator PanelBack(GameObject Panel)
+    {
+        for (int i = 0; i < 200; i++)
+        {
+            Panel.transform.localPosition -= new Vector3(4.0f, 0.0f, 0.0f);
+            yield return new WaitForSeconds(0.0001f);
+        }
+    }
+
+    //StageSelectパネルが右に移動
+    IEnumerator PanelForward(GameObject Panel)
+    {
+        for(int i=0;i<200;i++)
+        {
+            Panel.transform.localPosition += new Vector3(4.0f, 0.0f, 0.0f);
+            yield return new WaitForSeconds(0.0001f); 
+        }
+    }
+
+    IEnumerator CameraBack(GameObject Camera)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Camera.transform.localPosition += new Vector3(0.015f,0.0f,0.0f);
+            yield return new WaitForSeconds(0.002f);
+        }
+    }
+
+    IEnumerator CameraForward(GameObject Camera)
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            Camera.transform.localPosition -= new Vector3(0.015f, 0.0f, 0.0f);
+            yield return new WaitForSeconds(0.002f);
+        }
     }
 }
