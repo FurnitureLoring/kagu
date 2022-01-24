17733b7aa0e9de4e58e76f5adc920d4780319724
taniguchi-111 <h1202015031@hiratagakuen.onmicrosoft.com>
2022-01-24 11:43:33 +0900

    StageSelect画面の遷移を作成

--- kaguro-ru/Assets/Main/Script/StageSelect.cs
+++ kaguro-ru/Assets/Main/Script/StageSelect.cs
@@ -19,8 +19,8 @@ public class StageSelect : MonoBehaviour
     public void Back()
     {
         //StageSelectパネルが端を超えて移動しないように制御
-        if (Panel[0].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
-            Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
+        if (Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
+            Panel[2].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
         {
             for (int i = 0; i < 3; i++)
             {
@@ -33,8 +33,8 @@ public class StageSelect : MonoBehaviour
     public void Forward()
     {
         //StageSelectパネルが端を超えて移動しないように制御
-        if (Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
-            Panel[2].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
+        if (Panel[0].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f)||
+            Panel[1].transform.localPosition == new Vector3(0.0f, 0.0f, 0.0f))
         {
             for (int i = 0; i < 3; i++)
             {
@@ -47,9 +47,9 @@ public class StageSelect : MonoBehaviour
     //StageSelectパネルが左に移動
     IEnumerator PanelBack(GameObject Panel)
     {
-        for (int i = 0; i < 200; i++)
+        for (int i = 0; i < 100; i++)
         {
-            Panel.transform.localPosition -= new Vector3(4.0f, 0.0f, 0.0f);
+            Panel.transform.localPosition += new Vector3(8.0f, 0.0f, 0.0f);
             yield return new WaitForSeconds(0.0001f);
         }
     }
@@ -57,9 +57,9 @@ public class StageSelect : MonoBehaviour
     //StageSelectパネルが右に移動
     IEnumerator PanelForward(GameObject Panel)
     {
-        for(int i=0;i<200;i++)
+        for(int i=0;i<100;i++)
         {
-            Panel.transform.localPosition += new Vector3(4.0f, 0.0f, 0.0f);
+            Panel.transform.localPosition -= new Vector3(8.0f, 0.0f, 0.0f);
             yield return new WaitForSeconds(0.0001f); 
         }
     }
@@ -68,7 +68,7 @@ public class StageSelect : MonoBehaviour
     {
         for (int i = 0; i < 100; i++)
         {
-            Camera.transform.localPosition += new Vector3(0.015f,0.0f,0.0f);
+            Camera.transform.localPosition -= new Vector3(0.015f,0.0f,0.0f);
             yield return new WaitForSeconds(0.002f);
         }
     }
@@ -77,7 +77,7 @@ public class StageSelect : MonoBehaviour
     {
         for (int i = 0; i < 100; i++)
         {
-            Camera.transform.localPosition -= new Vector3(0.015f, 0.0f, 0.0f);
+            Camera.transform.localPosition += new Vector3(0.015f, 0.0f, 0.0f);
             yield return new WaitForSeconds(0.002f);
         }
     }
